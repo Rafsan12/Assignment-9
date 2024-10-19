aba5d0c32cb5667ddadaa2d112b3024f916711ad
Rafsan12 <rafsan.web.2022@gmail.com>
2024-10-19 23:02:16 +0600

    complete single incident card

---
+++ src/components/Incidents/Incident.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+export default function Incident({ incident }) {
+  const { location, city, price, image, category } = incident;
+  return (
+    <>
+      <div className="relative w-[306px] hover:scale-105 transition-transform'">
+        <img src={image} alt={location} className="w-full h-40 object-cover" />
+        <div className="p-4">
+          <h3 className="font-semibold text-lg">{location}</h3>
+          <p className="text-sm text-gray-500">{city}</p>
+          <p className="text-xl font-bold mt-2">{price}</p>
+        </div>
+        <div className="absolute top-2 right-2 bg-white rounded-full p-1 shadow-md">
+          <img className="text-xs font-medium" src={category} alt="" />
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/Incidents/Incidents.jsx
+++ src/components/Incidents/Incidents.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Incident from "./Incident";
 
 export default function Incidents() {
   const [cases, setCases] = useState([]);
@@ -20,7 +21,12 @@ export default function Incidents() {
     <>
       <div className="p-4">
         <p>Case: {cases.length}</p>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"></div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {cases.map((incident) => (
+            <Incident key={incident.id} incident={incident} />
+          ))}
+        </div>
+
         <div className="fixed bottom-4 right-4 bg-orange-500 text-white p-4 rounded-full">
           C
         </div>
